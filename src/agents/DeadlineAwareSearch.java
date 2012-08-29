aacd9fb6371d1f618b224fdcb92c8e282d3ace69
Rhys van der Waerden <rhys.vdw@gmail.com>
2012-08-29 16:23:24 +1000

    Change DAS to use new DasMapInfo construct

--- src/agents/DeadlineAwareSearch.java
+++ src/agents/DeadlineAwareSearch.java
@@ -20,15 +20,17 @@ import pplanning.simviewer.model.GridDomain;
 public class DeadlineAwareSearch implements PlanningAgent 
 {
 	private ComputedPlan plan;
-	
-	MapInfo mapinfo;
-	
-    private int stepNo = 0;
-    
-    private GridCell lastGoal = null;
-    
-    final private long MS_TO_NS_CONV_FACT = 1000000;
-    
+
+	DasMapInfo mapinfo;
+
+	// number of steps taken in current plan
+	private int stepNo = 0;
+
+	// goal state from previous call to getNextMove()
+	private GridCell lastGoal = null;
+
+	final private long MS_TO_NS_CONV_FACT = 1000000;
+
 	@Override
 	public GridCell getNextMove(GridDomain map, GridCell start, GridCell goal,
 			int stepLeft, long stepTime, long timeLeft) {
@@ -36,13 +38,22 @@ public class DeadlineAwareSearch implements PlanningAgent
 		try {
 			Trace.Enable(true);
 			GridCell nextStep = null;
-			
-			boolean bReplan = 
-					plan == null ||			// no last path stored, have yet notr planned before?
-					map.getChangedEdges().size() > 0 ||	// map has had changes 
-					!lastGoal.equals(goal) || // Goal has changed (equals not implemented?)
-					!plan.contains(start); // sNode is not in the path (sNode out of track)
-			
+
+			// recalculate plan if:
+			//  - no plan yet
+			//  - environment has changed
+			//  - goal has changed             TODO: check that equals actually works
+			//  - current node is not in path  TODO: is this a linear search?
+			//
+			// NOTE: I think Sebastian explicitly said that the goal wont move and the
+			// map wont change, so we could probably drop the last two conditions
+			boolean bReplan =
+				plan == null ||
+				map.getChangedEdges().size() > 0 ||
+				!lastGoal.equals(goal) ||
+				!plan.contains(start);
+
+
 			if (bReplan) 
 			{
 				long timeCurrent = System.nanoTime();
@@ -54,17 +65,17 @@ public class DeadlineAwareSearch implements PlanningAgent
 				lastGoal = goal;
 			}
 
+			// take next step in plan
 			if (stepNo < plan.getLength())
 			{
-
 				nextStep = (GridCell) plan.getStep(stepNo++);
-				
+
 				if (nextStep == null) 
 				{
 					Trace.print("next step is null!");
 				}
 			}
-			
+
 			return nextStep;
 
 		} 
@@ -78,7 +89,7 @@ public class DeadlineAwareSearch implements PlanningAgent
 	private ComputedPlan generatePlan(GridDomain map, GridCell start,
 			GridCell goal, long deadline) {
 
-		mapinfo = new MapInfo(map);
+		mapinfo = new DasMapInfo(map);
 
 		// initialize open set with start node
 		mapinfo.add(start, 0f, map.hCost(start, goal));
@@ -100,7 +111,7 @@ public class DeadlineAwareSearch implements PlanningAgent
 
 				// consider node if it can be entered and is not in closed list
 				if (map.isBlocked(neighbor) == false &&
-				    mapinfo.isClosed((GridCell) neighbor) == false) {
+						mapinfo.isClosed((GridCell) neighbor) == false) {
 
 					// get g cost of neighbor
 					float gCost = mapinfo.getGCost(current) + map.cost(current, neighbor);
@@ -113,11 +124,11 @@ public class DeadlineAwareSearch implements PlanningAgent
 						// NOTE: this can never happen with an admissible heuristic!
 						assert false;
 						/*
-						mapInfo.setCosts(neighbor, gCost, map.hCost(neighbor, goal));
-						mapInfo.setParent(neighbor, current);
-						*/
+							 mapInfo.setCosts(neighbor, gCost, map.hCost(neighbor, goal));
+							 mapInfo.setParent(neighbor, current);
+							 */
 					}
-				}
+						}
 			}
 		}
 		// no goal found
@@ -132,10 +143,10 @@ public class DeadlineAwareSearch implements PlanningAgent
 
 	@Override
 	public ArrayList<GridCell> expandedNodes() {
-		
+
 		ArrayList<GridCell> open = new ArrayList<GridCell>();
 		ArrayList<GridCell> closed = new ArrayList<GridCell>();
-		
+
 		if (mapinfo != null)
 		{
 			//Trace.print("Open Set has" + mapinfo.openCount() + " entries");
@@ -145,7 +156,7 @@ public class DeadlineAwareSearch implements PlanningAgent
 		}
 
 
-		
+
 		return(closed);
 	}
 
@@ -153,17 +164,17 @@ public class DeadlineAwareSearch implements PlanningAgent
 	public ArrayList<GridCell> unexpandedNodes() {	
 		ArrayList<GridCell> open = new ArrayList<GridCell>();
 		ArrayList<GridCell> closed = new ArrayList<GridCell>();
-		
+
 		if (mapinfo != null)
 		{
 			//Trace.print("Closed Set has" + mapinfo.closedCount() + " entries");
 			// Need to make this open/closed query into singleton style - it iterates through every tile twice, due 
 			// to being called at expanded nodes too!!
 			mapinfo.GetSearchSetsAsArrayList(open, closed);
-			
+
 
 		}
-		
+
 		return(open);
 
 	}
@@ -173,5 +184,5 @@ public class DeadlineAwareSearch implements PlanningAgent
 	public ComputedPlan getPath() {
 		return plan;
 	}
-	
+
 }
