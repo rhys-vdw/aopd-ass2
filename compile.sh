66b147e9e559fdb27d3d849acc917c81d5a32568
geoffsutcliffe <geoff.sutcliffe@gmail.com>
2012-09-17 03:15:27 +1000

    -Added jar file. -Removed guava.

--- compile.sh
+++ compile.sh
@@ -1,3 +1,3 @@
 #!
 clear
-javac -cp lib/JPathPlan-v1.6.jar:lib/Apparate-v2.8.jar:lib/guava-13.0.jar:. src/agents/*.java -d bin -Xlint:deprecation
+javac -cp lib/JPathPlan-v1.6.jar:lib/Apparate-v2.8.jar::. src/agents/*.java -d bin -Xlint:deprecation
--- run.sh
+++ run.sh
@@ -14,4 +14,4 @@ echo ================================================
 
 
 echo
-java -ea -cp bin/:lib/JPathPlan-v1.6.jar:lib/Apparate-v2.8.jar pplanning.simviewer.controller.Launcher $fileName
+java -cp bin/:lib/JPathPlan-v1.6.jar:lib/Apparate-v2.8.jar:lib/MyCoolAgent.jar pplanning.simviewer.controller.Launcher $fileName -
