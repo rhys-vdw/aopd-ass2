abc06ab63f31b3e13624258802dbd9c9f9e78583
geoffsutcliffe <geoff.sutcliffe@gmail.com>
2012-10-12 20:58:40 +1100

    -Updated to apparate 3.1 -Benchmarked C Timer - it is 3 times faster (see HRTimer.java)

--- src/agents/HRTimer.java
+++ src/agents/HRTimer.java
@@ -1,31 +1,40 @@
 package agents;
+import java.lang.management.ManagementFactory;
+import java.lang.management.ThreadMXBean;
+import java.lang.Math;
 
 public class HRTimer 
 {
 	private native void print();
 	
 	public native long getCurrentNanotime();
-
+	final static ThreadMXBean threadMX = ManagementFactory.getThreadMXBean();
 	public static void main(String[] args)
 	{
+		assert threadMX.isCurrentThreadCpuTimeSupported();
+		threadMX.setThreadCpuTimeEnabled(true);
 		HRTimer timer = new HRTimer();
-		timer.print();
-		timer.print();
-		timer.print();
-		timer.print();
-		timer.print();
-		System.out.println(timer.getCurrentNanotime());
-		System.out.println(timer.getCurrentNanotime());
-		System.out.println(timer.getCurrentNanotime());
-		System.out.println(timer.getCurrentNanotime());
-		System.out.println(timer.getCurrentNanotime());
-		System.out.println(timer.getCurrentNanotime());
-		timer.print();
-		timer.print();
-		timer.print();
-		timer.print();
-		timer.print();
+		
+		long conDiffsMX[] = new long[1000];
+		long conDiffsC[] = new long[1000];
+		
+		System.out.println("Now timing C Timer");
+		for (int n = 0; n < 1000; n++)
+		{
+			conDiffsC[n] = Math.abs(timer.getCurrentNanotime() - timer.getCurrentNanotime());
+			conDiffsMX[n] = Math.abs(threadMX.getCurrentThreadCpuTime() - threadMX.getCurrentThreadCpuTime());
+			
+		}
+		
+		for (int n = 0; n < 1000; n++)
+		{
+			// Tried around both ways - makes no difference.. C is faster :)
+			System.out.println("C: " + conDiffsC[n]);
+			System.out.println("MX: " + conDiffsMX[n]);
+			
+		}
 	}
+
 	static
 	{
 		try
